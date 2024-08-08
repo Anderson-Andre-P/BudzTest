dba17ff3e2cf799c437565a8b7bcecbfadf51e88
Anderson-Andre-P <eleu.ander@gmail.com>
2024-08-08 16:10:55 -0300

    fix: remove comments

--- budz_app/lib/main.dart
+++ budz_app/lib/main.dart
@@ -27,18 +27,3 @@ void main() {
     ),
   );
 }
-
-// class MainApp extends StatelessWidget {
-//   const MainApp({super.key});
-
-//   @override
-//   Widget build(BuildContext context) {
-//     return const MaterialApp(
-//       home: Scaffold(
-//         body: Center(
-//           child: Text('Hello World!'),
-//         ),
-//       ),
-//     );
-//   }
-// }
